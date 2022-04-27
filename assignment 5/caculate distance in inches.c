060372903e1ca992c910206a80d876b1dc0efa00
Rutikkale <101805007+Rutikkale@users.noreply.github.com>
2022-04-27 20:13:26 +0530

    Add files via upload

---
+++ assignment 5/caculate distance in inches.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	float i,n;
+	printf("enter distance: ");
+	scanf("%f",&n);
+	i=n/2.54;
+	printf("%f",i);
+	
+}
+	
+
+
+
---
+++ assignment 5/print value of s.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	float s=0;
+	for(i=1;i<=50;i++)
+	{
+	s=s+1/i;	
+	}
+	printf("value of s=%f",s);
+}
+
+
+
