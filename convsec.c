bc2056e9b44bf118755d1b60203e342b83c58717
Rutikkale <101805007+Rutikkale@users.noreply.github.com>
2022-04-28 14:07:47 +0530

    Add files via upload

---
+++ convsec.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+	int s,m,h;
+	printf("enter the seconds: \n");
+	scanf("%d",&s);
+	h=s/3600;
+	m=(s-(h*3600))/60;
+	s=s-((h*3600)+(m*60));
+	printf("hours: %d\n",h);
+	printf("minutes: %d\n",m);
+	printf("seconds: %d",s);
+	
+}
