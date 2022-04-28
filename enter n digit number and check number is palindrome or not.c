4e6c82c74fb792b3aab3d52a2461eab475628891
Rutikkale <101805007+Rutikkale@users.noreply.github.com>
2022-04-28 14:03:13 +0530

    Add files via upload

---
+++ enter n digit number and check number is palindrome or not.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int n,bak,num,rev=0;
+	printf("enter the number\n");
+	scanf("%d",&n);
+	bak=n;
+	while(n>0)
+	{
+		num=n%10;
+		rev=rev*10+num;
+		n=n/10;
+	}
+	if(bak==rev)
+	{
+		printf("palindrome number");
+	}
+	else
+	{
+		printf("not a palindrome number");
+	}
+	
+}
