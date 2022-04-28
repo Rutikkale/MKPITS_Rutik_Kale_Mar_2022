3917d5b55b28bb5600b047ad54f187fb86a87369
Rutikkale <101805007+Rutikkale@users.noreply.github.com>
2022-04-28 09:57:40 +0530

    Add files via upload

--- Untitled1.cpp
+++ Untitled1.cpp
@@ -1,22 +1,6 @@
-#include<stdio.h>
+#include<stdio>
 #include<conio.h>
-int main()
+void main()
 {
-	int choice;
-	//printf("enter 1 or 2 or 3 ");
-	//scanf("%d",choice);
-	switch(5)
-	{
-		case 1:
-			printf("in case 1");
-			break;
-	    case 2:
-	    	printf("in case 2");
-	    	break;
-	    case 3:
-	    	printf("in case 3");
-	    	break;
-	    	default:
-	    		printf("i am in default");
-	}
+	
 }
