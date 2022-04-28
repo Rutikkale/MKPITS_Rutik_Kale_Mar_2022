a139a6cdf938e45982809f04a26745901102deb3
Rutikkale <101805007+Rutikkale@users.noreply.github.com>
2022-04-28 14:15:17 +0530

    Add files via upload

---
+++ chech two numbers in pair is in ascending orderor indescending order _assi31.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2;
+	printf("enter two numbers in pair\n");
+	scanf("%d%d",&n1,&n2);
+	if(n1!=n2)
+	{
+		if(n1>n2)
+		{
+			printf("descending order");
+		}
+		else
+		{
+		printf("ascending oeder");
+		
+			
+		}
+	}
+}
