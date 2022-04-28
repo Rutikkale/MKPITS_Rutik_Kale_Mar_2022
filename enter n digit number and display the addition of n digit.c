2ac74a9a1e526a1f74f84923587cace81458a7e0
Rutikkale <101805007+Rutikkale@users.noreply.github.com>
2022-04-28 14:01:50 +0530

    Add files via upload

---
+++ enter n digit number and display the addition of n digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int n,num,bak,add=0;
+	printf("enter any number\n");
+	scanf("%d",&num);
+	bak=num;
+	while(num>0)
+	{
+	n=num%10;
+	add=add+n;
+	num=num/10;
+	}
+	printf("addition of %d = %d",bak,add);
+}
