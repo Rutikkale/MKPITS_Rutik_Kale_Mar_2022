0b10aa7039ad12c4557e743401f78c18e4a18f0a
Rutikkale <rutikkale17@gmail.com>
2023-04-03 10:41:41 +0530

    addtion of two number

---
+++ 3april2023/Addition_of_two_number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_of_two_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter your frist number");
+            int fristNO = int.Parse(Console.ReadLine());
+            Console.WriteLine("enter your second number");
+            int secondNO = int.Parse(Console.ReadLine());
+
+            int sum = fristNO + secondNO;
+            Console.WriteLine("total:{0}",sum);
+
+            Console.ReadLine();
+        }
+    }
+}
