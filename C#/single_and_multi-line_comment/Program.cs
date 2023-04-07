9ac47c95864153e425f7b3a47e97c94a80159048
Rutikkale <rutikkale17@gmail.com>
2023-04-07 12:23:13 +0530

    comments is c#

---
+++ C#/single_and_multi-line_comment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace single_and_multi_line_comment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //This is the single line comments
+            Console.WriteLine("helloWorld");
+            Console.ReadLine();
+            /* this is the multiline comments*/
+        }
+    }
+}
