e70fbbdb6f0190ac8812bbba5c4a0274cedb9e06
Rutikkale <rutikkale17@gmail.com>
2023-04-13 23:14:15 +0530

    my frist windows forms

---
+++ Windows Form/My_first_WindowsFormsApp1/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace My_first_WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label3.Text = textBox1.Text;
+            label3.Visible= true;
+
+
+           // MessageBox.Show("hello:" + textBox1.Text);
+            //MessageBox.Show("welcome to first windows forms ");
+        }
+    }
+}
